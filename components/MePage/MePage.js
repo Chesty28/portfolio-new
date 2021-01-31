23b9f9c55fad234132814ba9076d8e2b6e7742f2
Chesty <chestyczlp@gmail.com>
2021-01-31 04:39:13 +0100

    MePage done

--- components/MePage/MePage.js
+++ components/MePage/MePage.js
@@ -2,6 +2,9 @@ import React, { useContext } from 'react';
 import { Context } from '../../context/context';
 
 import Navbar from '../../UI/Navbar/Navbar';
+import Copyright from '../../UI/Copyright/Copyright';
+
+import s from './MePage.module.css';
 
 const MePage = () => {
     const context = useContext(Context);
@@ -16,9 +19,22 @@ const MePage = () => {
     };
 
     return (
-        <div style={style}>
+        <div className={s.mePage} style={style}>
             <Navbar />
-            me page
+            <h2 className={s.sectionHeader}>About me</h2>
+            <div className={s.meTextContainer}>
+                <p className={s.meText}><span className={s.mePinkSpan}>I am</span> 18 years and my passion is IT, especially <span className={s.meBlueSpan}>web development</span></p>
+                <p className={s.meText}><span className={s.mePinkSpan}>I am</span> junior <span className={s.meBlueSpan}>fullstack developer</span></p>
+                <p className={s.meText}><span className={s.mePinkSpan}>I am</span> using <span className={s.meBlueSpan}>MERN stack</span></p>
+                <p className={s.meText}><span className={s.mePinkSpan}>I am</span> working on my own startups <span className={s.meBlueSpan}>every day</span></p>
+            </div>
+            <div className={s.ballContainer}>
+                <div className={[s.ball, s.ball1].join(' ')}></div>
+                <div className={[s.ball, s.ball2].join(' ')}></div>
+                <div className={[s.ball, s.ball3].join(' ')}></div>
+                <div className={[s.ball, s.ball4].join(' ')}></div>
+            </div>
+            <Copyright />
         </div>
     );
 };
